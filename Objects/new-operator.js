3dd9f24728a8e55335dd2ffbba2b5165c4eb708b
Felipe Souza <contato@felipesouza.net>
2015-01-14 14:22:15 -0200

    new operator

---
+++ Objects/new-operator.js
@@ -0,0 +1,32 @@
+/*
+    new operator
+    -> Garante que o objeto referênciado pelo THIS seja retornado ao témino da função
+    -> Quando uma função é invocada por meio do new operator, é criado a instância do Object
+        e atribuida ao this. 
+*/
+
+var SiliconValley = function(criado, ano, elenco, pais, genero, duracao, descricao) {
+    this.criado = criado;
+    this.ano = ano;
+    this.elenco = elenco;
+    this.pais = pais;
+    this.genero = genero;
+    this.duracao = duracao;
+    this.descricao = descricao;
+}
+
+var informacoes = new SiliconValley(
+    "Mike Judge, John Altschuler, Dave Krinsky, Alec Berg"
+    ,2014
+    ,"Thomas Middleditch, T.J. Miller, Josh Brener ... + "
+    ,"EUA"
+    ,"Comédia"
+    ,"26 minutos"
+    ,"Richard (Thomas Middleditch), Big Head (Josh Brener), Gilfoyle (Martin Starr) e Dinesh (Kumail Nanjiani) são programadores tão inteligentes quanto introvertidos. Eles trabalham na companhia Hooli, no Vale do Silício, e vivem sob a custódia de Erlich, um milionário que os deixa morarem em sua casa de graça - desde que ele ganhe 10% dos lucros dos projetos dos garotos."
+);
+
+for(var i = 0; i < SiliconValley.length; i++){
+    console.log(informacoes)
+}
+
+console.log(informacoes);
